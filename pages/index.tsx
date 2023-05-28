4f7dbab37f5d45722a9468c830cdc1e0f5fdb976
Alexander Vishnevskiy <a.vishnevskiy@appyfurious.com>
2023-05-29 02:39:27 +0400

    fix theme colors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
+import * as process from 'process';
+
 import { NextPage } from 'next';
 
 import { DynamicPreview } from '@ui/preview';
 import { DynamicMain } from '@ui/pages/Main';
 
-const MainPage: NextPage = () => (true ? <DynamicMain /> : <DynamicPreview />);
+const MainPage: NextPage = () =>
+    process.env.NEXT_PUBLIC_NODE_ENV === 'development' ? <DynamicMain /> : <DynamicPreview />;
 
 export default MainPage;
