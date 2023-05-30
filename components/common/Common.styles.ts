ff584d6d0f4db58944a2294d555ac7321301c782
Alexander Vishnevskiy <a.vishnevskiy@appyfurious.com>
2023-05-31 01:09:17 +0400

    add main page images

--- components/common/Common.styles.ts
+++ components/common/Common.styles.ts
@@ -8,6 +8,10 @@ export const FlexRowCenter = styled(FlexRow)`
     align-items: center;
 `;
 
+export const FlexCenter = styled(FlexRowCenter)`
+    justify-content: center;
+`;
+
 export const FlexJCBetween = styled(FlexRow)`
     justify-content: space-between;
 `;
--- components/pages/Main/Main.tsx
+++ components/pages/Main/Main.tsx
@@ -3,13 +3,17 @@ import { ReactElement } from 'react';
 import PageStyleWrapper from '@ui/common/PageStyleWrapper';
 import EmbroideryIcon from '@icons/patterns/embroidery.svg';
 import EmbFireIcon from '@icons/patterns/emb-fire.svg';
+import Picture from '@ui/common/Picture';
 
 import { useAppStore } from '@/context';
+
+import useTranslation from 'next-translate/useTranslation';
 
 import * as S from './Main.styles';
 
 const Main = (): ReactElement => {
     const toggleColorMode = useAppStore((state) => state.toggleColorMode);
+    const { t } = useTranslation('common');
     return (
         <PageStyleWrapper widthType={'desktop'}>
             <S.MainLayout>
@@ -17,7 +21,19 @@ const Main = (): ReactElement => {
                     <EmbroideryIcon id={'emb-big'} alt={'embroidery'} />
                     <EmbFireIcon id={'emb-fire'} alt={'embroidery-fire'} />
                 </S.FirstImage>
-                <button onClick={() => toggleColorMode()}>Click me</button>
+                <S.MainColumn>
+                    <S.StyledTitle variant={'h2'}>{t('title')}</S.StyledTitle>
+                    <S.FrameContainer>
+                        <div id={'frame'} />
+                        <Picture
+                            src={'/static/images/avatars/we-1x.png'}
+                            src2x={'/static/images/avatars/we-2x.png'}
+                            srcWebp={'/static/images/avatars/we-1x.webp'}
+                            srcWebp2x={'/static/images/avatars/we-2x.webp'}
+                        />
+                    </S.FrameContainer>
+                    <button onClick={() => toggleColorMode()}>Click me</button>
+                </S.MainColumn>
                 <EmbroideryIcon id={'emb-big'} alt={'embroidery-big'} />
             </S.MainLayout>
         </PageStyleWrapper>
