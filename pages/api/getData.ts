28ced5815bef18752e62e9549528106aabd73377
Alexander Vishnevskiy <a.vishnevskiy@appyfurious.com>
2023-05-30 22:27:33 +0400

    update husky config

--- pages/api/getData.ts
+++ pages/api/getData.ts
@@ -7,7 +7,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     //Find the absolute path of the json directory
-    const jsonDirectory = path.join(process.cwd(), 'json');
+    const jsonDirectory = path.join(process.cwd(), 'tmp');
     //Read the json data file data.json
     const fileContents = await fs.readFile(jsonDirectory + '/data.json', 'utf8');
     //Return the content of the data file in json format
