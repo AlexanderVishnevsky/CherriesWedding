ed26531de543d1efe5620f9e4b0e501a9f3a2462
Alexander Vishnevskiy <a.vishnevskiy@appyfurious.com>
2023-06-11 02:25:58 +0400

    add Video.tsx

--- components/pages/Place/Place.styles.ts
+++ components/pages/Place/Place.styles.ts
@@ -1,3 +1,26 @@
 import { styled } from '@mui/material';
 
-export const Layout = styled('div')``;
+import { FlexColCenter } from '@ui/common/Common.styles';
+
+export const PlaceLayout = styled(FlexColCenter)`
+    width: 100%;
+
+    padding-top: 32px;
+    overflow: hidden auto;
+
+    ${({ theme }) => theme.breakpoints.down('tabletLarge')} {
+        padding-top: 64px;
+    }
+`;
+
+export const VideoContainer = styled('div')`
+    position: relative;
+    margin: 24px auto;
+    border-radius: 44px;
+
+    #bg-video {
+        width: 100%;
+        max-height: 620px;
+        border-radius: 44px;
+    }
+`;
