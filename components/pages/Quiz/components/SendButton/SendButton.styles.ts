b61f899151c11d3f1825c99cee6e81a21aff3ec9
Alexander Vishnevskiy <a.vishnevskiy@appyfurious.com>
2023-06-20 20:39:50 +0400

    fix UI

--- components/pages/Quiz/components/SendButton/SendButton.styles.ts
+++ components/pages/Quiz/components/SendButton/SendButton.styles.ts
@@ -1,5 +1,11 @@
-import { Button, styled } from '@mui/material';
+import { Box, Button, styled } from '@mui/material';
 
-export const Layout = styled(Button)`
+export const Layout = styled(Box)`
+    position: relative;
+`;
+
+export const StyledSendButton = styled(Button)`
     transition: all 0.2s ease-in-out;
+    background-color: ${({ theme }) => theme.palette.background.default};
+    z-index: 10;
 `;
